c48b015e449ff6b79577a8260667bd9c67362f93
Fabian König <Fabi@users.noreply.github.com>
2024-08-05 00:14:40 +0200

    Update to .NET 8 (#8)

--- WaypointCreatorGen2/WaypointCreator.cs
+++ WaypointCreatorGen2/WaypointCreator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Linq;
 using System.Threading.Tasks;
